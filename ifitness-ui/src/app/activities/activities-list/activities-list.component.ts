b69435732ec49d7119d7d0467025c245d2589737
BeatrizPichoneri <beatriz.pichoneri@aluno.ifsp.edu.br>
2023-09-22 11:18:44 -0300

    aula

---
+++ ifitness-ui/src/app/activities/activities-list/activities-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class ActivitiesListComponent {
+  
+  activities = [
+    { type: 'CORRIDA', activity_date: '18/08/2023', distance: 8.0, duration: 42, user: 'Fernando Duarte' },
+    { type: 'CORRIDA', activity_date: '16/08/2023', distance: 8.0, duration: 43, user: 'Fernando Duarte' },
+    { type: 'CAMINHADA', activity_date: '18/08/2023', distance: 5.0, duration: 55, user: 'Gislaine Rosales' }
+  ];
+}
---
+++ ifitness-ui/src/app/activities/activities.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+import { TooltipModule } from 'primeng/tooltip';
+import { ActivitiesListComponent } from './activities-list/activities-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ActivitiesListComponent
+  ],
+  imports: [
+    CommonModule,
+    TooltipModule,
+    ButtonModule,
+    TableModule
+  ],
+  exports: [
+    ActivitiesListComponent
+  ]
+})
+export class ActivitiesModule { }
+
